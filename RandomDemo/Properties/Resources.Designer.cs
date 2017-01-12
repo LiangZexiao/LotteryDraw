499fb7741a7190112fde50914d1b54d1f0227d01
LiangZexiao <psxyzz@126.com>
2017-01-12 17:33:18 +0800

    add project

--- RandomDemo/Properties/Resources.Designer.cs
+++ RandomDemo/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace RandomDemo.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
-        internal static System.Drawing.Bitmap bg_img {
-            get {
-                object obj = ResourceManager.GetObject("bg_img", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
         internal static System.Drawing.Bitmap temp {
             get {
                 object obj = ResourceManager.GetObject("temp", resourceCulture);
